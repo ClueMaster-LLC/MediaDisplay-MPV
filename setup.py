955aae7a2d6a6bc2442acbd0035a54a220025775
cluemaster-robert <79128397+cluemaster-robert@users.noreply.github.com>
2021-07-13 14:32:10 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name="cluemaster-mediadisplay",
+    name="cluemaster_display",
     version="0.4.6",
     description="Cluemaster Display",
     author="ClueMaster LLC",
     author_email="support@cluemaster.io",
-    packages=["cluemaster-mediadisplay"],
+    packages=["cluemaster_display"],
     install_requires=["PyQt5==5.14.1", "requests", "python-mpv"],
     scripts=["splash_screen", "authentication_screen", "loading_screen", "normal_screen", "game_idle", "master_overlay",
              "clue_containers", "platform_facts", "threads"]
