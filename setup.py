ffb22c736f772bbce9d576bbf75d607825b1e9bb
Mrittunjoy Sarkar <63890677+hiransarkar21@users.noreply.github.com>
2021-07-14 11:34:32 +0530

    Added , after find_packages()

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description="Cluemaster Display",
     author="Mrittunjoy Sarkar",
     author_email="mrittunjoysarkar21@gmail.com",
-    packages=find_packages()
+    packages=find_packages(),
     install_requires=["PyQt5==5.14.1", "requests", "python-mpv"],
     scripts=["splash_screen", "authentication_screen", "loading_screen", "normal_screen", "game_idle", "master_overlay",
              "clue_containers", "platform_facts", "threads"]
