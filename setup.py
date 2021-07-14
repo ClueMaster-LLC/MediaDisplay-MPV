23bcfa103dc72699b1570d9bc3c522cc324f0246
Mrittunjoy Sarkar <63890677+hiransarkar21@users.noreply.github.com>
2021-07-14 11:43:40 +0530

    Added .py extension after python file names.

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     author_email="mrittunjoysarkar21@gmail.com",
     packages=find_packages(),
     install_requires=["PyQt5==5.14.1", "requests", "python-mpv"],
-    scripts=["splash_screen", "authentication_screen", "loading_screen", "normal_screen", "game_idle", "master_overlay",
-             "clue_containers", "platform_facts", "threads"]
+    scripts=["splash_screen.py", "authentication_screen.py", "loading_screen.py", "normal_screen.py", "game_idle.py", "master_overlay.py",
+             "clue_containers.py", "platform_facts.py", "threads.py"]
 )
