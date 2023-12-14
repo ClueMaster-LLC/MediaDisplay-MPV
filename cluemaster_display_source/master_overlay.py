20ed163e14a65ea46abdc42d4b7263c3d77172fc
mrittunjoy <mrittunjoysarkar21@gmail.com>
2023-12-14 22:50:46 +0530

    added raise_() function to clue icons and timer windows. (unverified fix)

--- cluemaster_display_source/master_overlay.py
+++ cluemaster_display_source/master_overlay.py
@@ -93,6 +93,7 @@ class ClueContainer(QWidget):
         self.setAttribute(Qt.WA_TranslucentBackground)
         self.setAttribute(Qt.WA_NoSystemBackground)
         self.setCursor(Qt.BlankCursor)
+        self.raise_()
 
     def update_clues_used(self, used):
         """ this method is triggered after every 7 seconds and it checks if the number of clues used has changed in
@@ -332,7 +333,8 @@ class MasterOverlay(QWidget):
         self.setAttribute(Qt.WA_NoSystemBackground)
         self.setAttribute(Qt.WA_TranslucentBackground)
         self.setCursor(Qt.BlankCursor)
-        self.showFullScreen()
+        self.raise_()
+        # self.showFullScreen()
 
     def update_application_timer(self):
         """ this method is triggered as soon as the update_timer signal is emitted by the timer request thread, this
